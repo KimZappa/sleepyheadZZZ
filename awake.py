fe3f4d9425908fcd7655e66c52140aeee76a0d97
KimZappa <47152335+KimZappa@users.noreply.github.com>
2019-04-05 21:08:55 -0400

    Class to create awake student objects

---
+++ awake.py
@@ -0,0 +1,24 @@
+#Class to define sleepy student object
+import pygame
+WHITE = (255, 255, 255)
+
+
+class Awake(pygame.sprite.Sprite):
+    # This class represents a car. It derives from the "Sprite" class in Pygame.
+
+    def __init__(self, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        # Instead we could load a proper picture of a car...
+        self.image = pygame.image.load(r'C:\Users\KimZa\Desktop\Into to Python\game\awake.png').convert_alpha()
+
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()

3289301fa7e9951c871e9ce621297c137a08a325
KimZappa <47152335+KimZappa@users.noreply.github.com>
2019-04-06 00:39:56 -0400

    Update sleepyhead.py

--- sleepyhead.py
+++ sleepyhead.py
@@ -5,6 +5,15 @@ from teacher import Teacher
 from student import Student
 pygame.init()
 
+clock = pygame.time.Clock()
+
+black = (0, 0, 0)
+white = (255,255,255)
+red = (200,0,0)
+green = (0,200,0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
 size = (1200, 650)
 screen = pygame.display.set_mode(size)
 background = [210, 180, 140]
@@ -14,129 +23,239 @@ pygame.display.flip()
 
 desk = (124, 74, 59)
 
-#build all sleeping student sprites and set locations
-allSleepy_sprites_list = pygame.sprite.Group()
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
-sleepy_student1 = Sleepy(10, 10)
-sleepy_student1.rect.x = 20
-sleepy_student1.rect.y = 30
-allSleepy_sprites_list.add(sleepy_student1)
+def crash():
+    crash_sound = pygame.mixer.Sound(r'C:\Users\KimZa\Desktop\Into to Python\game\teacherCrash.wav')
+    pygame.mixer.Sound.play(crash_sound)
+    pygame.mixer.music.stop()
+    largeText = pygame.font.SysFont("comicsansms",115)
+    TextSurf, TextRect = text_objects("Busted! Game over!", largeText)
+    TextRect.center = ((1200 / 2), (650 / 2))
+    screen.blit(TextSurf, TextRect)
+    pygame.display.update()
 
-sleepy_student2 = Sleepy(10, 10)
-sleepy_student2.rect.x = 150
-sleepy_student2.rect.y = 300
-allSleepy_sprites_list.add(sleepy_student2)
+def win():
+    largeText = pygame.font.SysFont("comicsansms", 115)
+    TextSurf, TextRect = text_objects("You win!", largeText)
+    TextRect.center = ((1200 / 2), (650 / 2))
+    screen.blit(TextSurf, TextRect)
+    pygame.display.update()
 
-sleepy_student3 = Sleepy(10, 10)
-sleepy_student3.rect.x = 800
-sleepy_student3.rect.y = 350
-allSleepy_sprites_list.add(sleepy_student3)
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
 
-sleepy_student4 = Sleepy(10, 10)
-sleepy_student4.rect.x = 810
-sleepy_student4.rect.y = 100
-allSleepy_sprites_list.add(sleepy_student4)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(screen, ic,(x,y,w,h))
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
 
-sleepy_student5 = Sleepy(10, 10)
-sleepy_student5.rect.x = 150
-sleepy_student5.rect.y = 30
-allSleepy_sprites_list.add(sleepy_student5)
+def gameMenu():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-#build the awake student sprites
-allAwake_sprites_list = pygame.sprite.Group()
+        screen.fill(white)
+        largeText = pygame.font.SysFont("comicsansms", 115)
+        TextSurf, TextRect = text_objects("SleepyHeadzZZz", largeText)
+        TextRect.center = ((1200 / 2), (650 / 2))
+        screen.blit(TextSurf, TextRect)
 
-awake1 = Awake(10, 10)
-awake1.rect.x = 90
-awake1.rect.y = 180
-allAwake_sprites_list.add(awake1)
+        button("GO!", 150, 450, 100, 50, green, bright_green, main)
+        button("Quit", 550, 450, 100, 50, red, bright_red, quitgame)
 
-awake2 = Awake(10, 10)
-awake2.rect.x = 1100
-awake2.rect.y = 180
-allAwake_sprites_list.add(awake2)
+        pygame.display.update()
+        clock.tick(15)
 
-awake3 = Awake(10, 10)
-awake3.rect.x = 300
-awake3.rect.y = 270
-allAwake_sprites_list.add(awake3)
+def quitgame():
+    pygame.quit()
+    quit()
 
-awake4 = Awake(10, 10)
-awake4.rect.x = 400
-awake4.rect.y = 270
-allAwake_sprites_list.add(awake4)
+def main():
+    # build all sleeping student sprites and set locations
+    allSleepy_sprites_list = pygame.sprite.Group()
 
-awake5 = Awake(10, 10)
-awake5.rect.x = 400
-awake5.rect.y = 390
-allAwake_sprites_list.add(awake5)
+    sleepy_student1 = Sleepy(10, 10)
+    sleepy_student1.rect.x = 20
+    sleepy_student1.rect.y = 30
+    allSleepy_sprites_list.add(sleepy_student1)
 
-awake6 = Awake(10, 10)
-awake6.rect.x = 900
-awake6.rect.y = 500
-allAwake_sprites_list.add(awake6)
+    sleepy_student2 = Sleepy(10, 10)
+    sleepy_student2.rect.x = 150
+    sleepy_student2.rect.y = 300
+    allSleepy_sprites_list.add(sleepy_student2)
 
-awake7 = Awake(10, 10)
-awake7.rect.x = 700
-awake7.rect.y = 500
-allAwake_sprites_list.add(awake7)
+    sleepy_student3 = Sleepy(10, 10)
+    sleepy_student3.rect.x = 800
+    sleepy_student3.rect.y = 350
+    allSleepy_sprites_list.add(sleepy_student3)
 
-#build the teacher sprite
-allTeacher_sprites_list = pygame.sprite.Group()
-teacher = Teacher(10, 10)
-teacher.rect.x = 890
-teacher.rect.y = 180
-allTeacher_sprites_list.add(teacher)
+    sleepy_student4 = Sleepy(10, 10)
+    sleepy_student4.rect.x = 810
+    sleepy_student4.rect.y = 100
+    allSleepy_sprites_list.add(sleepy_student4)
 
-#build the stucent sprite
-allPlayer_sprites_list = pygame.sprite.Group()
-player = Student(10, 10)
-player.rect.x = 10
-player.rect.y = 550
-allPlayer_sprites_list.add(player)
+    sleepy_student5 = Sleepy(10, 10)
+    sleepy_student5.rect.x = 150
+    sleepy_student5.rect.y = 30
+    allSleepy_sprites_list.add(sleepy_student5)
 
+    # build the awake student sprites
+    allAwake_sprites_list = pygame.sprite.Group()
 
-run = True
-clock = pygame.time.Clock()
+    awake1 = Awake(10, 10)
+    awake1.rect.x = 90
+    awake1.rect.y = 180
+    allAwake_sprites_list.add(awake1)
 
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_x:  # Pressing the x Key will quit the game
+    awake2 = Awake(10, 10)
+    awake2.rect.x = 1100
+    awake2.rect.y = 180
+    allAwake_sprites_list.add(awake2)
+
+    awake3 = Awake(10, 10)
+    awake3.rect.x = 300
+    awake3.rect.y = 270
+    allAwake_sprites_list.add(awake3)
+
+    awake4 = Awake(10, 10)
+    awake4.rect.x = 400
+    awake4.rect.y = 270
+    allAwake_sprites_list.add(awake4)
+
+    awake5 = Awake(10, 10)
+    awake5.rect.x = 400
+    awake5.rect.y = 390
+    allAwake_sprites_list.add(awake5)
+
+    awake6 = Awake(10, 10)
+    awake6.rect.x = 900
+    awake6.rect.y = 500
+    allAwake_sprites_list.add(awake6)
+
+    awake7 = Awake(10, 10)
+    awake7.rect.x = 700
+    awake7.rect.y = 500
+    allAwake_sprites_list.add(awake7)
+
+    # build the teacher sprite
+    allTeacher_sprites_list = pygame.sprite.Group()
+    teacher = Teacher(10, 10)
+    teacher.rect.x = 690
+    teacher.rect.y = 180
+    allTeacher_sprites_list.add(teacher)
+
+    teacher2 = Teacher(10, 10)
+    teacher2.rect.x = 330
+    teacher2.rect.y = 150
+    allTeacher_sprites_list.add(teacher2)
+
+    # build the stucent sprite
+    allPlayer_sprites_list = pygame.sprite.Group()
+    player = Student(10, 10)
+    player.rect.x = 10
+    player.rect.y = 550
+    allPlayer_sprites_list.add(player)
+
+    # background music
+    pygame.mixer.music.load(r'C:\Users\KimZa\Desktop\Into to Python\game\UpbeatFunk.wav')
+    pygame.mixer.music.play(-1)
+
+    player.rect.x = 10
+    player.rect.y = 550
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_x:  # Pressing the x Key will quit the game
+                    run = False
+            elif len(allSleepy_sprites_list) == 0:
+                win()
+                pygame.time.wait(1200)
                 run = False
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player.moveLeft(5)
-    if keys[pygame.K_RIGHT]:
-        player.moveRight(5)
-    if keys[pygame.K_UP]:
-        player.moveUp(5)
-    if keys[pygame.K_DOWN]:
-        player.moveDown(5)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.moveLeft(5)
+        if keys[pygame.K_RIGHT]:
+            player.moveRight(5)
+        if keys[pygame.K_UP]:
+            player.moveUp(5)
+        if keys[pygame.K_DOWN]:
+            player.moveDown(5)
 
-    allSleepy_sprites_list.update()
-    allAwake_sprites_list.update()
-    allTeacher_sprites_list.update()
-    allPlayer_sprites_list.update()
-    screen.fill(background)
+        #if player collides with a teacher game ends
+        collideWithFirstTeacher = pygame.sprite.collide_rect(player,teacher)
+        if collideWithFirstTeacher == True:
+            crash()
+            pygame.time.wait(1200)
+            run = False
+        # if player collides with a teacher game ends
+        collideWithSecondTeacher = pygame.sprite.collide_rect(player, teacher2)
+        if collideWithSecondTeacher == True:
+            crash()
+            pygame.time.wait(1200)
+            run = False
 
-    #game layout
-    pygame.draw.rect(screen, desk, (100, 25, 200, 50))
-    pygame.draw.rect(screen, desk, (100, 50, 50, 175))
-    pygame.draw.rect(screen, desk, (900, 75, 215, 215))
-    pygame.draw.rect(screen, desk, (225, 350, 400, 80))
-    pygame.draw.rect(screen, desk, (725, 575, 200, 50))
-    pygame.draw.rect(screen, desk, (800, 450, 50, 175))
+         # if player colides with sleeping student remove it from screen
+        collideWithSleepy1 = pygame.sprite.collide_rect(player, sleepy_student1)
+        if collideWithSleepy1 == True:
+            allSleepy_sprites_list.remove(sleepy_student1)
 
-    allSleepy_sprites_list.draw(screen)
-    allAwake_sprites_list.draw(screen)
-    allTeacher_sprites_list.draw(screen)
-    allPlayer_sprites_list.draw(screen)
+        collideWithSleepy2 = pygame.sprite.collide_rect(player, sleepy_student2)
+        if collideWithSleepy2 == True:
+            allSleepy_sprites_list.remove(sleepy_student2)
 
-    pygame.display.flip()
-    clock.tick(60)
+        collideWithSleepy3 = pygame.sprite.collide_rect(player, sleepy_student3)
+        if collideWithSleepy3 == True:
+            allSleepy_sprites_list.remove(sleepy_student3)
 
+        collideWithSleepy4 = pygame.sprite.collide_rect(player, sleepy_student4)
+        if collideWithSleepy4 == True:
+             allSleepy_sprites_list.remove(sleepy_student4)
 
+        collideWithSleepy5 = pygame.sprite.collide_rect(player, sleepy_student5)
+        if collideWithSleepy5 == True:
+            allSleepy_sprites_list.remove(sleepy_student5)
+
+        allSleepy_sprites_list.update()
+        allAwake_sprites_list.update()
+        allTeacher_sprites_list.update()
+        allPlayer_sprites_list.update()
+        screen.fill(background)
+
+        #game layout
+        pygame.draw.rect(screen, desk, (100, 25, 200, 50))
+        pygame.draw.rect(screen, desk, (100, 50, 50, 175))
+        pygame.draw.rect(screen, desk, (900, 75, 215, 215))
+        pygame.draw.rect(screen, desk, (225, 350, 400, 80))
+        pygame.draw.rect(screen, desk, (725, 575, 200, 50))
+        pygame.draw.rect(screen, desk, (800, 450, 50, 175))
+
+        allSleepy_sprites_list.draw(screen)
+        allAwake_sprites_list.draw(screen)
+        allTeacher_sprites_list.draw(screen)
+        allPlayer_sprites_list.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+gameMenu()
+main()
 pygame.quit()
